7d1f0d7f288599bf99d2b67e95ab4c73b881813c
MasonDriggers <md20610@georgiasouthern.edu>
2023-11-14 14:45:44 -0500

    seperated calculate interest function

--- scripts.js
+++ scripts.js
@@ -1,27 +1,3 @@
-function compoundAmount(principal, rate, n, time) {
-    return principal * Math.pow(1 + rate/n, n*time);
-}
-
-function calculateInterest() {
-    const principal = parseFloat(document.getElementById("principal").value);
-    const rate = parseFloat(document.getElementById("rate").value);
-    const time = parseInt(document.getElementById("time").value);
-    const n = parseInt(document.getElementById("n").value);
-
-    const resultsTable = document.getElementById("results");
-    resultsTable.innerHTML = '<tr><th>Year</th><th>Amount</th></tr>';  // Clear and set headers
-    
-    for (let year = 1; year <= time; year++) {
-        const amount = compoundAmount(principal, rate, n, year);
-        
-        // Append results to table
-        const row = resultsTable.insertRow();
-        const cell1 = row.insertCell(0);
-        const cell2 = row.insertCell(1);
-        cell1.innerHTML = year;
-        cell2.innerHTML = amount.toFixed(2);
-    }
-}
 
 const API_URL = 'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=GDCUDJMS3MTAJKUG';
 
@@ -39,7 +15,7 @@ fetch(API_URL)
        const gainersList = document.getElementById('top_gainers');
        const losersList = document.getElementById('top_losers');
 
-        // For demonstration purposes, we just stringify the JSON. You should properly process the data and display it.
+       // stringifies the data for debugging
        // document.getElementById('data-container').textContent = JSON.stringify(data);
         
         data.top_gainers.forEach(stock => {
